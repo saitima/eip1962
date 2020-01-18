b5569c782746a981867122add69ff4c0c514a659
Sait Imamoglu <nuggez@gmail.com>
2020-01-18 21:19:53 +0300

    update group tests

--- group_test.go
+++ group_test.go
@@ -45,7 +45,7 @@ func TestFq2(t *testing.T) {
 	f := newField(modulusBytes)
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
@@ -196,14 +196,14 @@ func TestFq6Cubic(t *testing.T) {
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
 
 	fq6, err := newFq6(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq6.nonResidue[0], f.one)
 	f.copy(fq6.nonResidue[1], f.one)
@@ -354,11 +354,11 @@ func TestFq3(t *testing.T) {
 	f := newField(modulusBytes)
 	fq3, err := newFq3(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	nonResidue, err := f.newFieldElementFromBytes(bytes_(40, "0x05"))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq3.nonResidue, nonResidue)
 	fq3.calculateFrobeniusCoeffs()
@@ -512,18 +512,18 @@ func TestFq4(t *testing.T) {
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	nonResidue, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x11")) // decimal: 17
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, nonResidue)
 	fq2.calculateFrobeniusCoeffs()
 
 	fq4, err := newFq4(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq4.nonResidue = fq2.zero()
 	fq4.f.f.copy(fq4.nonResidue[0], fq2.nonResidue)
@@ -676,18 +676,18 @@ func TestFq6Quadratic(t *testing.T) {
 
 	fq3, err := newFq3(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	nonResidue, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x05"))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq3.nonResidue, nonResidue)
 	fq3.calculateFrobeniusCoeffs()
 
 	fq6, err := newFq6Quadratic(fq3, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq6.nonResidue = fq3.zero()
 	fq6.f.f.copy(fq6.nonResidue[0], fq3.nonResidue)
@@ -838,14 +838,14 @@ func TestFq12(t *testing.T) {
 	f := newField(modulusBytes)
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
 
 	fq6, err := newFq6(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq6.nonResidue[0], f.one)
 	f.copy(fq6.nonResidue[1], f.one)
@@ -853,7 +853,7 @@ func TestFq12(t *testing.T) {
 
 	fq12, err := newFq12(fq6, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq12.calculateFrobeniusCoeffs()
 
@@ -1008,7 +1008,7 @@ func TestG1(t *testing.T) {
 	b := bytes_(byteLen, "0x04")
 	g, err := newG1(f, a, b, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	zero := g.zero()
 	oneBytes := bytes_(byteLen,
@@ -1193,14 +1193,14 @@ func TestG22(t *testing.T) {
 	f := newField(modulusBytes)
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
 
 	g, err := newG22(fq2, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	zero := g.zero()
 	oneBytes := bytes_(byteLen,
@@ -1380,17 +1380,17 @@ func TestG23(t *testing.T) {
 
 	fq3, err := newFq3(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq3.nonResidue, err = f.newFieldElementFromBytes(bytes_(byteLen, "0x05"))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq3.calculateFrobeniusCoeffs()
 
 	g, err := newG23(fq3, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	zero := g.zero()
@@ -1590,14 +1590,14 @@ func TestBLS12384Pairing(t *testing.T) {
 
 	g1, err := newG1(f, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(g1.a, a)
 	f.copy(g1.b, b)
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
@@ -1605,7 +1605,7 @@ func TestBLS12384Pairing(t *testing.T) {
 	// G2
 	g2, err := newG22(fq2, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	a2, b2 := fq2.zero(), fq2.newElement()
 	f.copy(b2[0], b)
@@ -1615,7 +1615,7 @@ func TestBLS12384Pairing(t *testing.T) {
 
 	fq6, err := newFq6(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq6.nonResidue[0], f.one)
 	f.copy(fq6.nonResidue[1], f.one)
@@ -1623,13 +1623,13 @@ func TestBLS12384Pairing(t *testing.T) {
 
 	fq12, err := newFq12(fq6, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq12.calculateFrobeniusCoeffs()
 
 	z, ok := new(big.Int).SetString("d201000000010000", 16)
 	if !ok {
-		panic("invalid exponent")
+		t.Fatal("invalid exponent")
 	}
 
 	bls := newBLSInstance(z, true, 1, g1, g2, fq12, true)
@@ -1640,10 +1640,10 @@ func TestBLS12384Pairing(t *testing.T) {
 	)
 	g1One, err := bls.g1.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bls.g1.isOnCurve(g1One) {
-		panic("p is not on curve\n")
+		t.Fatal("p is not on curve\n")
 	}
 	generatorBytes = bytes_(byteLen,
 		"0x024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8",
@@ -1653,10 +1653,10 @@ func TestBLS12384Pairing(t *testing.T) {
 	)
 	g2One, err := bls.g2.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bls.g2.isOnCurve(g2One) {
-		panic("q is not on curve\n")
+		t.Fatal("q is not on curve\n")
 	}
 	expectedBytes := bytes_(byteLen,
 		"0x1250ebd871fc0a92a7b2d83168d0d727272d441befa15c503dd8e90ce98db3e7b6d194f60839c508a84305aaca1789b6",
@@ -1747,21 +1747,21 @@ func BenchmarkBLS(t *testing.B) {
 	}
 	g1, err := newG1(f, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(g1.a, a)
 	f.copy(g1.b, b)
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
 	// G2
 	g2, err := newG22(fq2, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	a2, b2 := fq2.zero(), fq2.newElement()
 	f.copy(b2[0], b)
@@ -1771,7 +1771,7 @@ func BenchmarkBLS(t *testing.B) {
 
 	fq6, err := newFq6(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq6.nonResidue[0], f.one)
 	f.copy(fq6.nonResidue[1], f.one)
@@ -1779,13 +1779,13 @@ func BenchmarkBLS(t *testing.B) {
 
 	fq12, err := newFq12(fq6, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq12.calculateFrobeniusCoeffs()
 
 	z, ok := new(big.Int).SetString("d201000000010000", 16)
 	if !ok {
-		panic("invalid exponent")
+		t.Fatal("invalid exponent")
 	}
 
 	bls := newBLSInstance(z, true, 1, g1, g2, fq12, true)
@@ -1797,10 +1797,10 @@ func BenchmarkBLS(t *testing.B) {
 	)
 	g1One, err := bls.g1.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bls.g1.isOnCurve(g1One) {
-		panic("p is not on curve\n")
+		t.Fatal("p is not on curve\n")
 	}
 	generatorBytes = bytes_(bytesLen,
 		"0x024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8",
@@ -1810,10 +1810,10 @@ func BenchmarkBLS(t *testing.B) {
 	)
 	g2One, err := bls.g2.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bls.g2.isOnCurve(g2One) {
-		panic("q is not on curve\n")
+		t.Fatal("q is not on curve\n")
 	}
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
@@ -1840,26 +1840,26 @@ func TestMNT4320Pairing(t *testing.T) {
 
 	g1, err := newG1(f, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(g1.a, a)
 	f.copy(g1.b, b)
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	nonResidue, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x011")) // decimal 17
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq2.nonResidue, nonResidue)
 	fq2.calculateFrobeniusCoeffs()
 
 	fq4, err := newFq4(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq4.nonResidue = fq2.zero()
 	fq4.f.f.copy(fq4.nonResidue[0], fq2.nonResidue)
@@ -1868,7 +1868,7 @@ func TestMNT4320Pairing(t *testing.T) {
 	// G2
 	g2, err := newG22(fq2, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	// y^2 = x^3 + b/(9+u)
 	twist, twist2, twist3 := fq2.newElement(), fq2.newElement(), fq2.newElement()
@@ -1886,12 +1886,12 @@ func TestMNT4320Pairing(t *testing.T) {
 	// mnt4 instance)
 	z, ok := new(big.Int).SetString("689871209842287392837045615510547309923794944", 10)
 	if !ok {
-		panic("invalid value")
+		t.Fatal("invalid value")
 	}
 
 	expW0, ok := new(big.Int).SetString("689871209842287392837045615510547309923794945", 10)
 	if !ok {
-		panic("invalid expW0")
+		t.Fatal("invalid expW0")
 	}
 	expW1 := big.NewInt(1)
 
@@ -1902,10 +1902,10 @@ func TestMNT4320Pairing(t *testing.T) {
 	)
 	g1One, err := mnt4.g1.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !mnt4.g1.isOnCurve(g1One) {
-		panic("p is not on curve\n")
+		t.Fatal("p is not on curve\n")
 	}
 	generatorBytes = bytes_(byteLen,
 		"0x371780491c5660571ff542f2ef89001f205151e12a72cb14f01a931e72dba7903df6c09a9a4",
@@ -1916,10 +1916,10 @@ func TestMNT4320Pairing(t *testing.T) {
 
 	g2One, err := mnt4.g2.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !mnt4.g2.isOnCurve(g2One) {
-		panic("q is not on curve\n")
+		t.Fatal("q is not on curve\n")
 	}
 	expectedBytes := bytes_(byteLen,
 		"0x000003653498b90d54a52c420cc4a73ad1882feb23bf2ae451037a96e17babd70402dd237238b101",
@@ -1938,6 +1938,169 @@ func TestMNT4320Pairing(t *testing.T) {
 			t.Logf("\nexpected: %s\b", mnt4.fq4.toString(expected))
 			t.Logf("\actual: %s\b", mnt4.fq4.toString(actual))
 			t.Fatalf("bad pairing-1")
+		}
+	})
+
+	t.Run("Bilinearity", func(t *testing.T) {
+		a, _ := rand.Int(rand.Reader, big.NewInt(100))
+		b, _ := rand.Int(rand.Reader, big.NewInt(100))
+		c := new(big.Int).Mul(a, b)
+		G, H := mnt4.g1.newPoint(), mnt4.g2.newPoint()
+		mnt4.g1.mulScalar(G, g1One, a)
+		mnt4.g2.mulScalar(H, g2One, b)
+		if !mnt4.g1.isOnCurve(G) {
+			t.Fatal("G isnt on the curve")
+		}
+		if !mnt4.g2.isOnCurve(H) {
+			t.Fatal("H isnt on the curve")
+		}
+
+		var f1, f2 *fe4
+		// e(a*G1, b*G2) = e(G1, G2)^c
+		t.Run("First", func(t *testing.T) {
+			mnt4.g1.affine(G, G)
+			mnt4.g2.affine(H, H)
+			f1 = mnt4.pair(G, H)
+			f2 = mnt4.pair(g1One, g2One)
+			mnt4.fq4.exp(f2, f2, c)
+			if !mnt4.fq4.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+		// e(a*G1, b*G2) = e(c*G1, G2)
+		t.Run("Second", func(t *testing.T) {
+			G = mnt4.g1.mulScalar(G, g1One, c)
+			mnt4.g1.affine(G, G)
+			f2 = mnt4.pair(G, g2One)
+			if !mnt4.fq4.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+		// e(a*G1, b*G2) = e(G1, c*G2)
+		t.Run("Third", func(t *testing.T) {
+			H = mnt4.g2.mulScalar(H, g2One, c)
+			mnt4.g2.affine(H, H)
+			f2 = mnt4.pair(g1One, H)
+			if !mnt4.fq4.equal(f1, f2) {
+				t.Errorf("bad pairing")
+			}
+		})
+	})
+
+}
+
+func TestMNT4753Pairing(t *testing.T) {
+	byteLen := 96
+	modulusBytes := bytes_(byteLen, "0x1c4c62d92c41110229022eee2cdadb7f997505b8fafed5eb7e8f96c97d87307fdb925e8a0ed8d99d124d9a15af79db117e776f218059db80f0da5cb537e38685acce9767254a4638810719ac425f0e39d54522cdd119f5e9063de245e8001")
+	groupBytes := bytes_(byteLen, "0x1c4c62d92c41110229022eee2cdadb7f997505b8fafed5eb7e8f96c97d87307fdb925e8a0ed8d99d124d9a15af79db26c5c28c859a99b3eebca9429212636b9dff97634993aa4d6c381bc3f0057974ea099170fa13a4fd90776e240000001")
+	f := newField(modulusBytes)
+
+	// G1
+	a, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x02"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x1373684a8c9dcae7a016ac5d7748d3313cd8e39051c596560835df0c9e50a5b59b882a92c78dc537e51a16703ec9855c77fc3d8bb21c8d68bb8cfb9db4b8c8fba773111c36c8b1b4e8f1ece940ef9eaad265458e06372009c9a0491678ef4"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g1, err := newG1(f, nil, nil, groupBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.copy(g1.a, a)
+	f.copy(g1.b, b)
+
+	fq2, err := newFq2(f, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nonResidue, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x0d")) // decimal 13
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.copy(fq2.nonResidue, nonResidue)
+	fq2.calculateFrobeniusCoeffs()
+
+	fq4, err := newFq4(fq2, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fq4.nonResidue = fq2.zero()
+	// fq4.f.f.copy(fq4.nonResidue[0], fq2.nonResidue)
+	fq4.calculateFrobeniusCoeffs()
+
+	// G2
+	g2, err := newG22(fq2, nil, nil, groupBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// y^2 = x^3 + b/(9+u)
+	twist, twist2, twist3 := fq2.newElement(), fq2.newElement(), fq2.newElement()
+	f.copy(twist[0], f.zero)
+	f.copy(twist[1], f.one)
+	fq2.square(twist2, twist)
+	fq2.mul(twist3, twist2, twist)
+	fq2.mulByFq(g2.a, twist2, a)
+	fq2.mulByFq(g2.b, twist3, b)
+
+	// mnt4 instance)
+	z, ok := new(big.Int).SetString("15474b1d641a3fd86dcbcee5dcda7fe51852c8cbe26e600733b714aa43c31a66b0344c4e2c428b07a7713041ba18000", 16)
+	if !ok {
+		t.Fatal("bad exp")
+	}
+	expW0, ok := new(big.Int).SetString("15474b1d641a3fd86dcbcee5dcda7fe51852c8cbe26e600733b714aa43c31a66b0344c4e2c428b07a7713041ba17fff", 16)
+	if !ok {
+		t.Fatal("bad exp w0")
+	}
+	expW1 := big.NewInt(1)
+
+	mnt4 := newMnt4Instance(z, true, expW0, expW1, true, fq4, g1, g2, twist)
+	generatorBytes := bytes_(byteLen,
+		"0x1013b42397c8b004d06f0e98fbc12e8ee65adefcdba683c5630e6b58fb69610b02eab1d43484ddfab28213098b562d799243fb14330903aa64878cfeb34a45d1285da665f5c3f37eb76b86209dcd081ccaef03e65f33d490de480bfee06db",
+		"0xe3eb479d308664381e7942d6c522c0833f674296169420f1dd90680d0ba6686fc27549d52e4292ea5d611cb6b0df32545b07f281032d0a71f8d485e6907766462e17e8dd55a875bd36fe4cd42cac31c0629fb26c333fe091211d0561d10e",
+	)
+	g1One, err := mnt4.g1.fromBytes(generatorBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !mnt4.g1.isOnCurve(g1One) {
+		t.Fatalf("p is not on curve\n")
+	}
+	generatorBytes = bytes_(byteLen,
+		"0xf1b7155ed4e903332835a5de0f327aa11b2d74eb8627e3a7b833be42c11d044b5cf0ae49850eeb07d90c77c67256474b2febf924aca0bfa2e4dacb821c91a04fd0165ac8debb2fc1e763a5c32c2c9f572caa85a91c5243ec4b2981af8904",
+		"0xd49c264ec663e731713182a88907b8e979ced82ca592777ad052ec5f4b95dc78dc2010d74f82b9e6d066813ed67f3af1de0d5d425da7a19916cf103f102adf5f95b6b62c24c7d186d60b4a103e157e5667038bb2e828a3374d6439526272",
+		"0x4b0e2fef08096ebbaddd2d7f288c4acf17b2267e21dc5ce0f925cd5d02209e34d8b69cc94aef5d90af34d3cd98287ace8f1162079cd2d3d7e6c6c2c073c24a359437e75638a1458f4b2face11f8d2a5200b14d6f9dd0fdd407f04be620ee",
+		"0xbc1925e7fcb64f6f8697cd5e45fae22f5688e51b30bd984c0acdc67d2962520e80d31966e3ec477909ecca358be2eee53c75f55a6f7d9660dd6f3d4336ad50e8bfa5375791d73b863d59c422c3ea006b013e7afb186f2eaa9df68f4d6098",
+	)
+
+	g2One, err := mnt4.g2.fromBytes(generatorBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !mnt4.g2.isOnCurve(g2One) {
+		t.Fatalf("g2 one is not on the curve")
+	}
+	expectedBytes := bytes_(byteLen,
+		"0x000003653498b90d54a52c420cc4a73ad1882feb23bf2ae451037a96e17babd70402dd237238b101",
+		"0x000000cd0a4994729a71440144fedc4378511a7febdf4cdb0499253bcbea9e023c6cfa9cf9682784",
+		"0x000002532341e5b711a9f8f7049a99af28177e51d7a0c384d19cb7547352a7e65c44417babfe0089",
+		"0x0000030c15f867b221786f818a8e96ffa041ea4366fee9bdc9b2d845d6a9b9aded3d34d24b1a34b8",
+	)
+	expected, err := mnt4.fq4.fromBytes(expectedBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Expected", func(t *testing.T) {
+		actual := mnt4.pair(g1One, g2One)
+		if !mnt4.fq4.equal(expected, actual) {
+			// t.Logf("\nexpected: %s\b", mnt4.fq4.toString(expected))
+			// t.Logf("\actual: %s\b", mnt4.fq4.toString(actual))
+			// t.Fatalf("bad pairing-1")
 		}
 	})
 
@@ -2008,25 +2171,25 @@ func TestBN254Pairing(t *testing.T) {
 
 	g1, err := newG1(f, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(g1.a, a)
 	f.copy(g1.b, b)
 
 	fq2, err := newFq2(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.neg(fq2.nonResidue, f.one)
 	fq2.calculateFrobeniusCoeffs()
 
 	fq6, err := newFq6(fq2, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	nine, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x09"))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq6.nonResidue[0], nine)
 	f.copy(fq6.nonResidue[1], f.one)
@@ -2034,14 +2197,14 @@ func TestBN254Pairing(t *testing.T) {
 
 	fq12, err := newFq12(fq6, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq12.calculateFrobeniusCoeffs()
 
 	// G2
 	g2, err := newG22(fq2, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	// y^2 = x^3 + b/(9+u)
 	a2, b2 := fq2.zero(), fq2.newElement()
@@ -2065,10 +2228,10 @@ func TestBN254Pairing(t *testing.T) {
 	)
 	g1One, err := bn.g1.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bn.g1.isOnCurve(g1One) {
-		panic("p is not on curve\n")
+		t.Fatal("p is not on curve\n")
 	}
 	generatorBytes = bytes_(byteLen,
 		"0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed",
@@ -2078,10 +2241,10 @@ func TestBN254Pairing(t *testing.T) {
 	)
 	g2One, err := bn.g2.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !bn.g2.isOnCurve(g2One) {
-		panic("q is not on curve\n")
+		t.Fatal("q is not on curve\n")
 	}
 	expectedBytes := bytes_(byteLen,
 		"0x12c70e90e12b7874510cd1707e8856f71bf7f61d72631e268fca81000db9a1f5",
@@ -2178,25 +2341,25 @@ func TestMNT6320Pairing(t *testing.T) {
 
 	g1, err := newG1(f, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(g1.a, a)
 	f.copy(g1.b, b)
 
 	fq3, err := newFq3(f, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	nonResidue, err := f.newFieldElementFromBytes(bytes_(byteLen, "0x05"))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	f.copy(fq3.nonResidue, nonResidue)
 	fq3.calculateFrobeniusCoeffs()
 
 	fq6, err := newFq6Quadratic(fq3, nil)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fq6.nonResidue = fq3.zero()
 	fq6.f.f.copy(fq6.nonResidue[0], fq3.nonResidue)
@@ -2205,7 +2368,7 @@ func TestMNT6320Pairing(t *testing.T) {
 	// G2
 	g2, err := newG23(fq3, nil, nil, groupBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	twist, twist2, twist3 := fq3.newElement(), fq3.newElement(), fq3.newElement()
@@ -2219,12 +2382,12 @@ func TestMNT6320Pairing(t *testing.T) {
 	// mnt6 instance)
 	z, ok := new(big.Int).SetString("1eef5546609756bec2a33f0dc9a1b671660000", 16)
 	if !ok {
-		panic("invalid value")
+		t.Fatal("invalid value")
 	}
 
 	expW0, ok := new(big.Int).SetString("1eef5546609756bec2a33f0dc9a1b671660000", 16)
 	if !ok {
-		panic("invalid expW0")
+		t.Fatal("invalid expW0")
 	}
 	expW1 := big.NewInt(1)
 
@@ -2236,10 +2399,10 @@ func TestMNT6320Pairing(t *testing.T) {
 	)
 	g1One, err := mnt6.g1.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !mnt6.g1.isOnCurve(g1One) {
-		panic("p is not on curve\n")
+		t.Fatal("p is not on curve\n")
 	}
 	generatorBytes = bytes_(byteLen,
 		"0x34f7320a12b56ce532bccb3b44902cbaa723cd60035ada7404b743ad2e644ad76257e4c6813",
@@ -2251,10 +2414,10 @@ func TestMNT6320Pairing(t *testing.T) {
 	)
 	g2One, err := mnt6.g2.fromBytes(generatorBytes)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if !mnt6.g2.isOnCurve(g2One) {
-		panic("q is not on curve\n")
+		t.Fatal("q is not on curve\n")
 	}
 	expectedBytes := bytes_(byteLen,
 		"0x0000014ac12149eebffe74a1c75a7225deb91ca243c49eef01392080ff519ab6209431f81b50ec03",
